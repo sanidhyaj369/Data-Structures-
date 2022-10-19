ee4cf37fcfa65d2caf546448a8aa485ebec8b696
Sanidhya Jain <98800950+sanidhyaj369@users.noreply.github.com>
2022-10-19 11:14:46 +0530

    Create H-Index.cpp

---
+++ Leetcode/H-Index.cpp
@@ -0,0 +1,10 @@
+int hIndex(vector<int>& cit) {
+        sort(cit.begin(),cit.end());
+        int n=cit.size(),count=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(n-i<=cit[i])
+                count++;
+        }
+        return count;
+    }
